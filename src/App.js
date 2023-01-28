4af26a400067d46cf30bafcabe387e82c3c3d23c
developer-sahadat <mdridoysikder862@gmail.com>
2023-01-29 00:10:31 +0600

    design completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
+import Blog from "./Component/Blog/Blog";
+import Contact from "./Component/Contact/Contact";
+import Features from "./Component/Features/Features";
+import Footer from "./Component/Footer";
 import Header from "./Component/Head/Header";
 import Home from "./Component/Hero/Home";
+import Portfolio from "./Component/Portfolio/Portfolio";
+import Resume from "./Component/Resume/Resume";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Features />
+      <Portfolio />
+      <Resume />
+      <Blog />
+      <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/Component/Portfolio/Portfolio_data.js
@@ -0,0 +1,46 @@
+import portfolio from "../../Assets/Images/portfolio-large-04-340x250.jpg";
+const Portfolio_data = [
+  {
+    id: 1,
+    category: "DEVELOPMENT",
+    totalLike: "600",
+    title: "The services provide for design ",
+    image: portfolio,
+  },
+  {
+    id: 2,
+    category: "APPLICATION",
+    totalLike: "750",
+    title: "Mobile app landing design & maintain",
+    image: portfolio,
+  },
+  {
+    id: 3,
+    category: "PHOTOSHOP",
+    totalLike: "630",
+    title: "Logo design creativity & Application ",
+    image: portfolio,
+  },
+  {
+    id: 4,
+    category: "FIGMA",
+    totalLike: "360",
+    title: "Mobile app landing design & Services",
+    image: portfolio,
+  },
+  {
+    id: 5,
+    category: "WEB DESIGN",
+    totalLike: "280",
+    title: "Design for tecnology & services",
+    image: portfolio,
+  },
+  {
+    id: 6,
+    category: "WEB DESIGN",
+    totalLike: "690",
+    title: "App for tecnology & services",
+    image: portfolio,
+  },
+];
+export default Portfolio_data;
